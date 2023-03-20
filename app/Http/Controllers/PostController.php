9015cb4529b16eccf7bb3101ee0ba51c6d18d61b
shotarosemba <shotaro.semba@gmail.com>
2023-03-21 00:41:29 +0900

    make posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
+
+
 class PostController extends Controller
 {
-public function index(Post $post){return $post->get();}
+public function index(Post $post){return view('posts/index')->with(['posts' => $post->get()]);
 }
+}
+?>

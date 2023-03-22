56a08aae72778bd9364a2739330e8e300cbe4259
shotarosemba <shotaro.semba@gmail.com>
2023-03-22 16:20:42 +0900

    post make

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-public function index(Post $post){return view('posts/index')->with(['posts' => $post->get()]);
+public function index(Post $post){return view('posts/index')->with(['posts' => $post->getPaginateBylimit(1)]);
 }
 }
 ?>
